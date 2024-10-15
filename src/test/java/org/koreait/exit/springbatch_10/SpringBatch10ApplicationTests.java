aec137865a729e31f22e318742e104ba3c00f820
s3496h <s3496h@gmail.com>
2024-10-15 14:42:32 +0900

    프로젝트 세팅, 관련 스키마 생성

---
+++ src/test/java/org/koreait/exit/springbatch_10/SpringBatch10ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.koreait.exit.springbatch_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatch10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
