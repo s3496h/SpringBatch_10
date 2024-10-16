8f32b2ca34a8bf15fd9ab5679858ff0bbab67c85
s3496h <s3496h@gmail.com>
2024-10-16 15:59:57 +0900

    샘플 회원 4명 생성

---
+++ src/main/java/com/koreait/exam/springbatch_10/job/HelloWorld/HelloWorldJobConfig.java
@@ -0,0 +1,73 @@
+package com.koreait.exam.springbatch_10.job.HelloWorld;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class HelloWorldJobConfig {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job helloWorldJob() {
+        return jobBuilderFactory.get("helloWorldJob")
+//                .incrementer(new RunIdIncrementer()) // 강제로 매번 다른 ID를 실행할 때 파라미터로 부여
+                .start(helloWorldStep1())
+                .next(helloWorldStep2())
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step helloWorldStep1() {
+        return stepBuilderFactory
+                .get("helloWorldStep1")
+                .tasklet(helloWorldStep1Tasklet())
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet helloWorldStep1Tasklet() {
+        return (stepContribution, chunkContext) -> {
+            System.out.println("헬로월드 111111111111111!!!");
+            return RepeatStatus.FINISHED;
+        };
+    }
+
+    @Bean
+    @JobScope
+    public Step helloWorldStep2() {
+        return stepBuilderFactory
+                .get("helloWorldStep2")
+                .tasklet(helloWorldStep2Tasklet())
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet helloWorldStep2Tasklet() {
+        return (stepContribution, chunkContext) -> {
+            System.out.println("헬로월드 222222222222!!!");
+
+            if(false){
+                throw new Exception("실패 : 헬로월드 태스클릿 2 실패");
+            }
+
+            return RepeatStatus.FINISHED;
+        };
+    }
+
+
+}
