2225548c59e007c75dbf58799b20f88d768facef
s3496h <s3496h@gmail.com>
2024-10-21 14:16:15 +0900

    between 범위 정산

--- src/main/java/com/koreait/exam/springbatch_10/app/order/repository/OrderItemRepository.java
+++ src/main/java/com/koreait/exam/springbatch_10/app/order/repository/OrderItemRepository.java
@@ -1,7 +1,12 @@
 package com.koreait.exam.springbatch_10.app.order.repository;
 
 import com.koreait.exam.springbatch_10.app.order.entity.OrderItem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+    Page<OrderItem> findAllByIdLessThan(long id, Pageable pageable);
+    Page<OrderItem> findAllByIdBetween(long fromId, long toId, Pageable pageable);
 }
--- src/main/java/com/koreait/exam/springbatch_10/job/makeCalculateOrderItem/makeCalculateOrderItemJobConfig.java
+++ src/main/java/com/koreait/exam/springbatch_10/job/makeCalculateOrderItem/makeCalculateOrderItemJobConfig.java
@@ -4,10 +4,6 @@ import com.koreait.exam.springbatch_10.app.order.entity.CalculateOrderItem;
 import com.koreait.exam.springbatch_10.app.order.entity.OrderItem;
 import com.koreait.exam.springbatch_10.app.order.repository.CalculateOrderItemRepository;
 import com.koreait.exam.springbatch_10.app.order.repository.OrderItemRepository;
-import com.koreait.exam.springbatch_10.app.product.entity.Product;
-import com.koreait.exam.springbatch_10.app.product.entity.ProductBackup;
-import com.koreait.exam.springbatch_10.app.product.repository.ProductBackupRepository;
-import com.koreait.exam.springbatch_10.app.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
@@ -21,27 +17,35 @@ import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.data.RepositoryItemReader;
 import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Sort;
+
 import java.util.Arrays;
 import java.util.Collections;
+
 @Configuration
 @RequiredArgsConstructor
 @Slf4j
 public class makeCalculateOrderItemJobConfig {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
+
     private final OrderItemRepository orderItemRepository;
     private final CalculateOrderItemRepository calculateOrderItemRepository;
+
     @Bean
     public Job makeCalculateOrderItemJob(Step makeCalculateOrderItemStep1, CommandLineRunner initData) throws Exception {
+
         initData.run();
+
         return jobBuilderFactory.get("makeCalculateOrderItemJob")
                 .start(makeCalculateOrderItemStep1)
                 .build();
     }
+
     @Bean
     @JobScope
     public Step makeCalculateOrderItemStep1(
@@ -56,32 +60,42 @@ public class makeCalculateOrderItemJobConfig {
                 .writer(calculateOrderItemWriter)
                 .build();
     }
+
     @StepScope
     @Bean
-    public RepositoryItemReader<OrderItem> orderItemReader() {
+    public RepositoryItemReader<OrderItem> orderItemReader(
+            @Value("#{jobParameters['fromId']}") Long fromId,
+            @Value("#{jobParameters['toId']}") Long toId
+    ) {
         return new RepositoryItemReaderBuilder<OrderItem>()
                 .name("orderItemReader")
                 .repository(orderItemRepository)
-                .methodName("findAll")
+                .methodName("findAllByIdBetween")
                 .pageSize(100)
-                .arguments(Arrays.asList())
+                .arguments(Arrays.asList(fromId, toId))
                 .sorts(Collections.singletonMap("id", Sort.Direction.ASC))
                 .build();
     }
+
     @StepScope
     @Bean
     public ItemProcessor<OrderItem, CalculateOrderItem> orderItemToCalculateOrderItemProcessor() {
         return orderItem -> new CalculateOrderItem(orderItem);
     }
+
     @StepScope
     @Bean
     public ItemWriter<CalculateOrderItem> calculateOrderItemWriter() {
         return items -> items.forEach(item -> {
             CalculateOrderItem oldCalculateOrderItem = calculateOrderItemRepository.findByOrderItemId(item.getOrderItem().getId()).orElse(null);
-            if(oldCalculateOrderItem != null) {
+
+            if (oldCalculateOrderItem != null) {
                 calculateOrderItemRepository.delete(oldCalculateOrderItem);
             }
+
             calculateOrderItemRepository.save(item);
         });
     }
+
+
 }
