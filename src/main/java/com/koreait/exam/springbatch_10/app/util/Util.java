18d57ec0b4eda025951ba72c4509f96b91d403e4
s3496h <s3496h@gmail.com>
2024-10-22 14:35:40 +0900

    날짜 문자열로 LocalDateTime 만들어 주는 함수

--- src/main/java/com/koreait/exam/springbatch_10/app/util/Util.java
+++ src/main/java/com/koreait/exam/springbatch_10/app/util/Util.java
@@ -6,18 +6,22 @@ import java.time.format.DateTimeFormatter;
 public class Util {
     public static class date {
         public static int getEndDayOf(int year, int month) {
-            String yearStr = year + "-";
+       /*     String yearStr = year + "-";
             String monthStr = month + "";
             if (monthStr.length() == 1) {
                 monthStr = "0" + monthStr;
             }
-            String yearMonth = yearStr + monthStr;
+            String yearMonth = yearStr + monthStr;*/
+            String yearMonth = year + "-" + "%02d".formatted(month);
             return getEndDayOf(yearMonth);
         }
         public static int getEndDayOf(String yearMonth) {
             LocalDate convertedDate = LocalDate.parse(yearMonth + "-01", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
             convertedDate = convertedDate.withDayOfMonth(convertedDate.getMonth().length(convertedDate.isLeapYear()));
             return convertedDate.getDayOfMonth();
+        }
+        public static LocalDateTime parse(String pattern, String texttDate) {
+            return LocalDateTime.parse(texttDate, DateTimeFormatter.ofPattern(pattern));
         }
     }
 }
